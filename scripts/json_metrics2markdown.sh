ebcb2523e18b93e8b0a12b520e5554377cb2c0c3
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-06-22 10:32:00 +0200

    update scripts

--- scripts/json_metrics2markdown.sh
+++ scripts/json_metrics2markdown.sh
@@ -3,16 +3,16 @@
 INP=$1
 
 jq -r '
-  (["prog","jit","etiss_arch","arch","abi","mips","sim_time","sim_cycles"] | @tsv),
-  (.[] | [.prog,.jit,.etiss_arch,.arch,.abi,.mips,.Simulation_Time,.CPU_cycle] | @tsv)
+  (["prog","toolchain","jit","etiss_arch","arch","abi","mips","sim_time","sim_cycles"] | @tsv),
+  (.[] | [.prog,.toolchain,.jit,.etiss_arch,.arch,.abi,.mips,.Simulation_Time,.CPU_cycle] | @tsv)
 ' $INP |
 awk '
 BEGIN { FS=OFS="\t" }
 NR==1 {
-    print "| "$1" | "$2" | "$3" | "$4"/"$5" | "$6" | "$7" | "$8" |"
-    print "|---|---|---|---|---|---|---|"
+    print "| "$1" | "$2" | "$3" | "$4"/"$5" | "$6" | "$7" | "$8" | "$9" |"
+    print "|---|---|---|---|---|---|---|---|"
     next
 }
 {
-    print "| "$1" | "$2" | "$3" | "$4"/"$5" | "$6" | "$7"s | "$8" |"
+    print "| "$1" | "$2" | "$3" | "$4"/"$5" | "$6" | "$7"s | "$8" | "$9" |"
 }'
